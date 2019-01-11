2322d1579b803cd1f2e0051e05cb257df5fe4618
BL <45978711+Lonsdaleb2@users.noreply.github.com>
2019-01-11 18:47:11 +0000

    Add files via upload

---
+++ trade_class.py
@@ -0,0 +1,11 @@
+
+class trade_goods:
+
+    def __init__(self, type, availability, tons, base_price, purchase_dm_list, sale_dm_list, examples): #these are the attributes of the student. Data types
+        self.type = type
+        self.availability = availability
+        self.tons = tons
+        self.base_price = base_price
+        self.purchase_dm_list = purchase_dm_list
+        self.sale_dm_list = sale_dm_list
+        self.examples = examples
